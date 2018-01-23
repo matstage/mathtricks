3d7f9df2c09311cb1465352b92315cac8ac8ba6a
Mats Tage Axelsson <matstage@gmail.com>
2018-01-24 01:17:41 +0200

    Second commit, CircleCI

--- app/src/main/java/com/example/matstage/mathtricks/MainActivity.java
+++ app/src/main/java/com/example/matstage/mathtricks/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.matstage.mathtricks;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mathChoiceButton;
+        mathChoiceButton = findViewById(R.id.spinner);
+        mathChoiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
+
+        Button executeBtn = findViewById(R.id.executeButton);
+        executeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumberEditText = (EditText) findViewById(R.id.firstNumberEditText);
+                EditText secondNumberEditText = (EditText) findViewById(R.id.secondNumberEditText);
+
+                TextView resultTextview = (TextView) findViewById(R.id.resulttextView);
+
+                int num1 = Integer.parseInt(firstNumberEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumberEditText.getText().toString());
+                int result = num1 + num2;
+
+                resultTextview.setText(result + "");
+            }
+        });
+
     }
 }
